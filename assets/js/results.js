6bff2cbde155f2dbb758d31710438b500973f0e3
Benjamin Perdido <bperdido@uci.edu>
2025-01-27 20:17:12 -0800

    implemented conditional CSS; adjusted indentation

--- assets/js/results.js
+++ assets/js/results.js
@@ -11,30 +11,30 @@ final_score.textContent = `${total_score}`;
 // creates variable for depression level
 let depression_level = '';
 
-// determines depression level
+// determines depression level and CSS
 rubric(total_score);
 
 // displays depression severity
 const mood_name = document.querySelector('#mood_name');
 mood_name.textContent = depression_level;
 
-// function for determining depression level
+// function for determining depression level and CSS
 function rubric (score) {
     const result = document.querySelector('#result'); // selects result CSS link
-    if (score >= 1 && score <= 4) {
+    if (score >= 0 && score <= 4) {
         depression_level = 'minimal depression'; // score 1-4: minimal depression
-        result.href = "./assets/css/result1.css"; // applies result1 CSS to HTML
+        result.href = "./assets/css/result1.css"; // applies result1 CSS to HTML - currently blue
     } else if (score >= 5 && score <= 9) {
         depression_level = 'mild depression'; // score 5-9: mild depression
-        result.href = "./assets/css/result1.css"; // applies result1 CSS to HTML
+        result.href = "./assets/css/result2.css"; // applies result2 CSS to HTML - currently green
     } else if (score >= 10 && score <= 14) {
         depression_level = 'moderate depression'; // score 10-14: moderate depression
-        result.href = "./assets/css/result1.css"; // applies result1 CSS to HTML
+        result.href = "./assets/css/result3.css"; // applies result3 CSS to HTML - currently yellow
     } else if (score >= 15 && score <= 19) {
         depression_level = 'moderately severe depression'; // score 15-19: moderately severe depression
-        result.href = "./assets/css/result1.css"; // applies result1 CSS to HTML
+        result.href = "./assets/css/result4.css"; // applies result4 CSS to HTML - currently red
     } else if (score >= 20 && score <= 27) {
         depression_level = 'severe depression'; // score 20-27: severe depression
-        result.href = "./assets/css/result1.css"; // applies result1 CSS to HTML
+        result.href = "./assets/css/result5.css"; // applies result5 CSS to HTML - currently gray
     }
 }
